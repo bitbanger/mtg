8bf2dc353fe415f289b735dd367da10df26653ce
Lane <lanelawley@gmail.com>
2025-03-02 23:52:37 -0500

    x

--- parse_collection.py
+++ parse_collection.py
@@ -4,8 +4,10 @@ import json
 import os
 import re
 import requests
+import time
 import traceback
 
+from collections import defaultdict
 from io import StringIO
 from lane import *
 from tqdm import *
@@ -50,12 +52,69 @@ def scryfall_csv_row(set_name, card):
 	res = f'"{set_name.upper()}","{card.cn}","{name}","{f}"'
 	return res
 
-def parse_cards(fn):
-	txt = ll_read(fn)
-	lines = ll_lines(txt)
-	set_name = lines[0]
-	card_strs = lines[1].split('.')
+def file2sets(fn):
+	sets = defaultdict(list)
+	set_name = ''
+	set_lines = []
+	for line in ll_lines(ll_read(fn)):
+		# This line has cards for sure
+		if '.' in line:
+			set_lines.append(line)
+			continue
 
+		# This can't be a set name
+		if len(line) > 3:
+			set_lines.append(line)
+			continue
+
+		# No letters? No set name...I think
+		if len(set(ll_alpha.upper()).intersection(set(line))) == 0:
+			set_lines.append(line)
+			continue
+
+		# OK, it's a set name. Commit the lines
+		# to the last set name, if any, & reset
+		if set_name:
+			sets[set_name].extend(set_lines)
+			set_name = ''
+			set_lines = []
+		set_name = line
+	
+	# Finish up
+	if set_lines:
+		sets[set_name].extend(set_lines)
+	
+	ret_sets = {}
+	for set_name, set_lines in sets.items():
+		# Combine any card lines into one line, separated by dots
+		set_txt = '.'.join(set_lines)
+
+		# Remove repeated dots
+		while '..' in set_txt:
+			set_txt = set_txt.replace('..', '.')
+		
+		# Split cards out by dot
+		card_strs = set_txt.split('.')
+
+		# Parse the cards
+		cards = cardstrs2cards(set_name, card_strs)
+
+		# Add the cards
+		ret_sets[set_name] = cards
+
+	return ret_sets
+
+def files2sets(fns):
+	sets = defaultdict(list)
+	for fn in fns:
+		if os.path.basename(fn).startswith('_'):
+			continue
+		for set_name, cards in file2sets(fn).items():
+			sets[set_name].extend(cards)
+	
+	return sets
+
+def cardstrs2cards(set_name, card_strs):
 	cards = []
 	for cs in card_strs:
 		cn = ll_only_nums(cs)
@@ -70,33 +129,38 @@ def parse_cards(fn):
 				card.token = True
 		cards.append(card)
 
-	return set_name, cards
+	return cards
 
-def fetch_card_data(input_files):
-	rows = {}
-	counts = {}
-	pbar_1 = tqdm(input_files, bar_format='{desc}{bar}')
-	for i, fn in enumerate(pbar_1):
-		set_name = fn.split(".")[0].split("/")[-1].upper()
-		pbar_1.set_description(f'Set {i+1} of {len(input_files)} ({set_name})')
-		if not fn.endswith('.txt'):
+def sets2rows(sets, delay=False):
+	rows, counts = {}, {}
+
+	set_cards = []
+	for set_name in sets.keys():
+		for card in sets[set_name]:
+			set_cards.append((set_name, card))
+
+	pbar = tqdm(set_cards, bar_format='{desc}{bar}')
+	for i, (set_name, card) in enumerate(pbar):
+		pbar.set_description_str(f'Card {i+1} of {len(set_cards)} (current set: {set_name}) ')
+
+		if str(card) in counts:
+			counts[str(card)] += 1
 			continue
-		set_name, cards = parse_cards(fn)
-		pbar_2 = tqdm(cards, bar_format='{desc}{bar}', leave=False)
-		for j, card in enumerate(pbar_2):
-			pbar_2.set_description(f'Card {j+1} of {len(cards)}')
-			if str(card) in counts:
-				counts[str(card)] += 1
-				continue
 
-			try:
-				row = scryfall_csv_row(set_name, card)
-				rows[str(card)] = row
-				counts[str(card)] = 1
-			except:
-				f.write(f'lookup failed on {set_name} {card.cn}' + '\n')
-				print(f'lookup failed on {set_name} {card.cn}', flush=True)
-				print(traceback.format_exc(), flush=True)
+		try:
+			if delay:
+				time.sleep(0.01)
+			row = scryfall_csv_row(set_name, card)
+			rows[str(card)] = row
+			counts[str(card)] = 1
+		except:
+			f.write(f'lookup failed on {set_name} {card.cn}' + '\n')
+			print(f'lookup failed on {set_name} {card.cn}', flush=True)
+			print(traceback.format_exc(), flush=True)
+
+		if i == len(set_cards)-1:
+			pbar.set_description_str(f'Card {len(set_cards)} of {len(set_cards)} ')
+	
 
 	return rows, counts
 
@@ -110,7 +174,7 @@ def write_row(file, row, count, card_kingdom=False):
 			n = '%s (%04d)' % (n, int(cn))
 		file.write(f'"{count}","{ed}","{n}","{f}"\n')
 
-def write_file(out_fn, rows, counts, append=False, card_kingdom=False):
+def write_file(out_fn, rows, counts, append=False, card_kingdom=False, exclude_manuals=False):
 	with open(out_fn, 'a+' if append else 'w+') as f:
 		if not card_kingdom:
 			f.write('"Count","Edition","Collector Number","Name","Foil"\n')
@@ -119,20 +183,25 @@ def write_file(out_fn, rows, counts, append=False, card_kingdom=False):
 			row = f'"{count}",' + rows[card]
 			write_row(f, row, count, card_kingdom=card_kingdom)
 
-		# Write the manual cards in, too
-		for line in ll_lines(ll_read('in/_manual.csv', 'r'))[1:]:
-			count = ll_csv_row(line)[0]
-			write_row(f, line, count, card_kingdom=card_kingdom)
+		if (not card_kingdom) and (not exclude_manuals):
+			# Write the manual cards in, too
+			for line in ll_lines(ll_read('in/_manual.csv', 'r'))[1:]:
+				count = ll_csv_row(line)[0]
+				write_row(f, line, count, card_kingdom=card_kingdom)
 
 def main():
 	ap = argparse.ArgumentParser()
 	ap.add_argument('input_files', nargs='+')
 	ap.add_argument('-a', '--append', action='store_true')
+	ap.add_argument('-d', '--delay', action='store_true')
+	ap.add_argument('-e', '--exclude-manuals', action='store_true')
 	args = ap.parse_args()
 
-	rows, counts = fetch_card_data(args.input_files)
-	write_file('out/collection.csv', rows, counts, append=args.append)
-	write_file('out/ck_collection.csv', rows, counts, append=args.append, card_kingdom=True)
+	sets = files2sets(args.input_files)
+	rows, counts = sets2rows(sets, delay=args.delay)
+
+	write_file('out/collection.csv', rows, counts, append=args.append, exclude_manuals=args.exclude_manuals)
+	write_file('out/ck_collection.csv', rows, counts, append=args.append, card_kingdom=True, exclude_manuals=True)
 
 if __name__ == '__main__':
 	main()
